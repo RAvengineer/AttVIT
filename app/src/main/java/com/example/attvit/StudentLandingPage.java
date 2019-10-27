4612fd4b129b820c7405214b6d3bd0a0c8d5c949
Rahul <rahul.rb554.rb3@gmail.com>
2019-10-27 23:22:21 +0530

    Add details in Student Landing Page

--- app/src/main/java/com/example/attvit/StudentLandingPage.java
+++ app/src/main/java/com/example/attvit/StudentLandingPage.java
@@ -2,13 +2,26 @@ package com.example.attvit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class StudentLandingPage extends AppCompatActivity {
+    TextView tvName, tvRegNo;
+    SharedPreferences userDetails = getSharedPreferences("UserDetails",MODE_PRIVATE);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_landing_page);
     }
+
+    // <editor-fold default="collapsed" desc="initializeComponents">
+    private void initializeComponents() {
+        tvName = findViewById(R.id.tvName);
+        tvName.setText(userDetails.getString("user_name","Doctor Strange"));
+        tvRegNo = findViewById(R.id.tvRno);
+        tvRegNo.setText(userDetails.getString("user_id","14000605"));
+    }
+    // </editor-fold>
 }
