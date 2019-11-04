cd696ecfe8e68e1ae05adffcb925d771ed384c2d
Adarsh Jaiswal <geniusadarsh3@gmail.com>
2019-11-04 13:36:47 +0530

    Added modify attendance

---
+++ app/src/main/java/com/example/attvit/TeacherModifyAttdBySlot.java
@@ -0,0 +1,14 @@
+package com.example.attvit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TeacherModifyAttdBySlot extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher_modify_attd_by_slot);
+    }
+}
